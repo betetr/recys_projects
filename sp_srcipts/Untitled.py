3f1a3c5a87e55246348dd42a86ccbf2d01f7be15
betetr <13697132117@163.com>
2018-01-26 14:32:27 +0800

    nothing

--- sp_srcipts/Untitled.py
+++ sp_srcipts/Untitled.py
@@ -10,7 +10,8 @@ import numpy as np
 from datetime import datetime
 
 if __name__=="__main__":
-    df = pd.read_csv("E:\\projects\\recys\\dataset\\0_999_user_info.csv")
+    # df = pd.read_csv("E:\\recys_projects\\dataset\\0_999_user_info.csv")
+    df = pd.read_csv("/home/xuyayuan/recys_projects/dataset/0_999_user_info.csv")
     del df["Unnamed: 0"]
     df['time'] = pd.to_datetime(df['time'])
     user_days = df[df['user_id']==98047837][df['time'] >= datetime(2014,11,2)][df['time'] < datetime(2014,11,3)]
